f24f89ab3207f0a3c868fa60f2320c27d33f4df5
kojiro.iizuka <kojiro.iizuka@gunosy.com>
2019-08-28 18:01:05 +0900

    imple multi thread

--- gensim/models/poincare.py
+++ gensim/models/poincare.py
@@ -47,6 +47,7 @@ import logging
 from numbers import Integral
 import sys
 import time
+import threading
 
 import numpy as np
 from collections import defaultdict, Counter
@@ -526,7 +527,7 @@ class PoincareModel(utils.SaveLoad):
         all_indices = [self._sample_negatives(node) for node in nodes]
         return all_indices
 
-    def _train_on_batch(self, relations, check_gradients=False):
+    def _train_on_batch(self, relations, worker_results, check_gradients=False):
         """Perform training for a single training batch.
 
         Parameters
@@ -545,6 +546,7 @@ class PoincareModel(utils.SaveLoad):
         all_negatives = self._sample_negatives_batch(relation[0] for relation in relations)
         batch = self._prepare_training_batch(relations, all_negatives, check_gradients)
         self._update_vectors_batch(batch)
+        worker_results.append(batch)
         return batch
 
     @staticmethod
@@ -628,8 +630,6 @@ class PoincareModel(utils.SaveLoad):
             >>> model.train(epochs=50)
 
         """
-        if self.workers > 1:
-            raise NotImplementedError("Multi-threaded version not implemented yet")
         # Some divide-by-zero results are handled explicitly
         old_settings = np.seterr(divide='ignore', invalid='ignore')
 
@@ -674,32 +674,55 @@ class PoincareModel(utils.SaveLoad):
             Useful for debugging, doesn't compare by default.
 
         """
-        if self.workers > 1:
-            raise NotImplementedError("Multi-threaded version not implemented yet")
+
         for epoch in range(1, epochs + 1):
             indices = list(range(len(self.all_relations)))
             self._np_random.shuffle(indices)
             avg_loss = 0.0
             last_time = time.time()
+            workers = []
+            worker_results = []
+            total_batch_num = 0
             for batch_num, i in enumerate(range(0, len(indices), batch_size), start=1):
-                should_print = not (batch_num % print_every)
                 check_gradients = bool(check_gradients_every) and (batch_num % check_gradients_every) == 0
                 batch_indices = indices[i:i + batch_size]
                 relations = [self.all_relations[idx] for idx in batch_indices]
-                result = self._train_on_batch(relations, check_gradients=check_gradients)
-                avg_loss += result.loss
-                if should_print:
-                    avg_loss /= print_every
-                    time_taken = time.time() - last_time
-                    speed = print_every * batch_size / time_taken
-                    logger.info(
-                        'training on epoch %d, examples #%d-#%d, loss: %.2f'
-                        % (epoch, i, i + batch_size, avg_loss))
-                    logger.info(
-                        'time taken for %d examples: %.2f s, %.2f examples / s'
-                        % (print_every * batch_size, time_taken, speed))
-                    last_time = time.time()
-                    avg_loss = 0.0
+
+                workers.append(
+                    threading.Thread(
+                        target=self._train_on_batch,
+                        args=(relations, worker_results,),
+                        kwargs={'check_gradients': check_gradients}
+                    )
+                )
+
+                if len(workers) == self.workers or batch_num == len(indices) / batch_size:
+                    for thread in workers:
+                        thread.daemon = True  # make interrupting the process with ctrl+c easier
+                        thread.start()
+
+                    for thread in workers:
+                        thread.join()
+
+                    workers = []
+                    worker_results = []
+
+                    for result in worker_results:
+                        total_batch_num += 1
+                        avg_loss += result.loss
+                        should_print = not (total_batch_num % print_every)
+                        if should_print:
+                            avg_loss /= print_every
+                            time_taken = time.time() - last_time
+                            speed = print_every * batch_size / time_taken
+                            logger.info(
+                                'training on epoch %d, examples #%d-#%d, loss: %.2f'
+                                % (epoch, i, i + batch_size, avg_loss))
+                            logger.info(
+                                'time taken for %d examples: %.2f s, %.2f examples / s'
+                                % (print_every * batch_size, time_taken, speed))
+                            last_time = time.time()
+                            avg_loss = 0.0
 
 
 class PoincareBatch(object):
